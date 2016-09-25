254d0f97c09dd4ef35f572ee7d881c8688b384fe
HomMarkHunt <honmaso@gmail.com>
2016-09-25 13:52:43 +0900

    cnfirm directory structure.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: node bot.js
+worker: node bin/index.js
---
+++ scripts/reply.js
@@ -0,0 +1,12 @@
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+});
